92ec1a92af87c26bef912080f1acbc896dd81ef6
phongnx1 <xuanphong09t1@gmail.com>
2017-01-30 16:36:22 +0700

    Update CheckLoginAction.java

--- src/action/CheckLoginAction.java
+++ src/action/CheckLoginAction.java
@@ -25,7 +25,8 @@ public class CheckLoginAction extends Action{
 		CheckLoginForm checkLoginForm = (CheckLoginForm) form;
 		String userName = checkLoginForm.getUserName();
 		String passWord = checkLoginForm.getPassword();
-		System.out.println("userName:"+userName);
+		
+		// Check login
 		CheckLoginBO checkLoginBO = new CheckLoginBO();
 		if (checkLoginBO.checkLogin(userName, passWord)!=0)
 			return mapping.findForward("success");
